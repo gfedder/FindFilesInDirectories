bde3813af9e6a23c1b2bccbf82a3ec929477aa12
gFedz <38161940+gfedder@users.noreply.github.com>
2023-02-15 10:07:17 -0600

    Upload of script

---
+++ FindFilesPS.ps1
@@ -0,0 +1,14 @@
+﻿$rootDirectory = 'D:\Repos\'
+$minFileSize = 1024#*1024*1024
+$runDate = (Get-Date).ToString('yyyyMMddHHmmss')
+$logFile = $PSScriptRoot + '\FileFindings' + $runDate + '.txt'
+$count = 0
+
+"File Size`tCreation DateTime`tFile Name`tPath" >> $logFile
+
+Get-ChildItem $rootDirectory -Attributes !Directory -Recurse | Where-Object { $_.Length -gt $minFileSize } | ForEach-Object {
+    $(($_.Length/1MB).ToString("#.###") + " GB`t" + $_.CreationTime + "`t" + $_.Name + "`t" + $_.DirectoryName) >> $logFile
+    $count++
+}
+
+"Total Files Found: " + $count >> $logFile
